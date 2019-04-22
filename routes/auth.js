2fc70056e046640e5a2e552231d4fbcc1f13a03c
Diego Cornejo <diegof.cornejo@gmail.com>
2019-04-22 09:13:26 -0600

    Update hash functions to use async/await

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,26 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
-var bcrypt = require('bcrypt');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-var createHash = function (password) {
-    console.log(password);
-    return new Promise(resolve => {
-        bcrypt.hash(password, saltRounds, function (err, hash) {
-            if (hash) {
-                resolve(hash);
-            } else {
-                resolve(err);
-            }
-        });
-    });
+var createHash = async function (password) {
+    try {
+        let hash = await bcrypt.hash(password, saltRounds);
+        return hash;
+    } catch (e) {
+        Error(`Error: ${e}`);
+    }
 };
 
-var compareHash = function (password, hash) {
-    return new Promise(resolve => {
-        bcrypt.compare(password, hash, function (err, res) {
-            if (res) {
-                resolve(res);
-            } else {
-                resolve(err);
-            }
-        });
-    });
+var compareHash = async function (password, hash) {
+    try {
+        let match = await bcrypt.compare(password, hash);
+        return match;
+    } catch (e) {
+        Error(`Error: ${e}`);
+    }
 };
 
 /* login */
@@ -36,7 +29,6 @@ router.get('/', async function (req, res, next) {
     //Simulate DB response
     let dbusername = 'diego';
     let dbhash = '$2b$10$4Sh.ut83.kUlSJIJGW94POymbn/ZJDPfo3Bz4V3G/o4dEr8lw.JBq';
-
     let passwordMatch = await compareHash(req.query.password, dbhash);
     if (req.query.username == dbusername && passwordMatch) {
         res.sendStatus(200);
